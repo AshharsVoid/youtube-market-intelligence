d9139c0ba0b03ec1b2235742ca99104f951cb659
AshharsVoid <ashhar.d10@gmail.com>
2026-07-05 19:29:05 +0530

    Google sheets auth

--- src/config.py
+++ src/config.py
@@ -1,17 +1,17 @@
-from pathlib import Path
 from dotenv import load_dotenv
 import os
 
-# Project root directory
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Load the .env file explicitly
-dotenv_path = BASE_DIR / ".env"
-load_dotenv(dotenv_path)
+load_dotenv()
 
 YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
+GOOGLE_SERVICE_ACCOUNT = os.getenv("GOOGLE_SERVICE_ACCOUNT")
+GOOGLE_SHEET_ID = os.getenv("GOOGLE_SHEET_ID")
 
 if not YOUTUBE_API_KEY:
-    raise ValueError(
-        f"YOUTUBE_API_KEY not found.\nExpected .env at: {dotenv_path}"
-    )
+    raise ValueError("YOUTUBE_API_KEY not found.")
+
+if not GOOGLE_SERVICE_ACCOUNT:
+    raise ValueError("GOOGLE_SERVICE_ACCOUNT not found.")
+
+if not GOOGLE_SHEET_ID:
+    raise ValueError("GOOGLE_SHEET_ID not found.")
